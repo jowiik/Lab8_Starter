1047b6432dad51aa50e69ad4dd9641dfb5fdc6a0
Joseph Kim <jowii.kim@gmail.com>
2025-05-26 20:03:31 -0700

    finished without index link

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,17 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js')
+        .then((registration) => {
+          console.log('Service worker registered', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service worker registration failed', error);
+        });
+    });
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -68,6 +79,10 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const storedRecipes = localStorage.getItem('recipes');
+  if (storedRecipes) {
+    return JSON.parse(storedRecipes);
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -77,6 +92,23 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  let recipes = [];
+  return new Promise(async (resolve, reject) => {
+    try {
+      for (let i = 0; i < RECIPE_URLS.length; i++) {
+        const response = await fetch(RECIPE_URLS[i]);
+        const data = await response.json();
+        recipes.push(data);
+      }
+
+      saveRecipesToStorage(recipes);
+      resolve(recipes);
+
+    } catch (error) {
+      console.error("Couldn't fetch recipe", error);
+      reject(error);
+    }
+  });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
